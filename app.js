f1ab65023f4e906007405e14ff0331f2dbfe02b9
techxtor <techxtor472@gmail.com>
2022-03-20 17:42:48 +0545

    add free to use website"

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const http = require('http');
 const port = process.env.PORT || 3000
 
-const httpServer = http.createServer((req, res) =>{
-    res.setHeader("Content-Type", "application/json");
-    res.writeHead(200);
-    res.end(JSON.stringify({
-        Messgae: `Hello there ! It's manish here.`,
-      }));
+const httpServer = http.createServer((req, res) => {
+    if (req.uri === '/') {
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        res.end(JSON.stringify({
+            Messgae: `Welcome to Techxtor free api. Fetch details: /api/details`,
+        }));
+    }
+    else if (req.uri === '/api/details') {
+        res.setHeader("Content-Type", "application/json");
+        res.writeHead(200);
+        res.end(JSON.stringify({
+            Names: [
+                'Manish',
+                'Bibhash',
+                'Subhash'
+            ],
+            Email: [
+                'manish@test.test',
+                'bibhash@test.test',
+                'subhash@test.test'
+            ],
+            Messgae: 'This is a free to use api'
+        }));
+    }
 })
 
-httpServer.listen(port,  (err) => {
+httpServer.listen(port, (err) => {
     if (err) console.log(err);
     console.log(`Server is running on port : ${port}`);
 });
